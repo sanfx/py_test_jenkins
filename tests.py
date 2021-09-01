d019045ccc570761efb3e0379cfed7b29588fe1e
Sanjeev Kumar <skysan@gmail.com>
2021-09-01 23:10:24 +0100

    Update tests.py

--- tests.py
+++ tests.py
@@ -15,7 +15,7 @@ class SimpleTest(unittest.TestCase):
         self.assertEqual(10, 7 + 3)
 
     def test_fail(self):
-        self.assertEqual(11, 7 + 3)
+        self.assertEqual(10, 7 + 3)
 
 
 if __name__ == '__main__':
